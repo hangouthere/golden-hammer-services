1306035bd9de8cff8f099645e46d59b481108dd7
nfgCodex <nerdfoundrygaming+git@gmail.com>
2022-01-25 00:06:34 -0600

    * Added Jaeger tracing monitoring to env * Silly formatting of moleculer.config.js * Updated tracer to Jaeger * Demonstrable tracing added to listenToChannel socket handler

--- services/config-io.js
+++ services/config-io.js
@@ -10,7 +10,18 @@ module.exports = {
             mappingPolicy: 'restrict'
           },
           listenToChannel(incomingValue, next) {
+            debugger;
+
+            const span = this.$service.broker.tracer.startSpan('PubSub: Channel Listen - Register', {
+              service: 'pub-sub:chat',
+              tags: {
+                socket: { id: this.client.conn.id, remoteAddress: this.client.conn.remoteAddress }
+              }
+            });
+
             console.log('Incoming Value:', incomingValue);
+
+            span.finish();
 
             return next({
               gotIt: true,
