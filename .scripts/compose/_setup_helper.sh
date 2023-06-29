5e0a147a29222d88ff259afc181a8e10d6e637af
CodexHere <codexhere@outlook.com>
2023-06-29 00:00:19 -0500

    * Setup now will chown first to reduce the amount of files needed to   chown overall * Volume mount empty .git file to avoid runtime "not a git project"   error * Got rid of erroneous gh-shared dev dep, now relies on being a peer-dep   for sub-dependencies

--- .scripts/compose/_setup_helper.sh
+++ .scripts/compose/_setup_helper.sh
@@ -2,6 +2,6 @@
 
 npm i && \
 printf "Copying node_modules to folders: %s\n" "$(find /_vols/* -maxdepth 1 -type d)" && \
+chown -R 1000.1000 /app && \
 find /_vols/* -print0 -maxdepth 1 -type d | xargs -I {} rm -rf "{}/*" && \
-find /_vols/* -print0 -maxdepth 1 -type d | xargs -I {} cp -R /app/node_modules/* "{}/" > /dev/null && \
-chown -R 1000.1000 /_vols/ /app
+find /_vols/* -print0 -maxdepth 1 -type d | xargs -I {} cp -R /app/node_modules/* "{}/" > /dev/null
