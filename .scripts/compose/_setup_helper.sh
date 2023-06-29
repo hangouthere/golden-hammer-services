92263b4f76ff427e98242993564a764a476d2a56
CodexHere <codexhere@outlook.com>
2023-06-29 01:13:48 -0500

    Fix script to actually work :P

--- .scripts/compose/_setup_helper.sh
+++ .scripts/compose/_setup_helper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 npm i && \
-printf "Copying node_modules to folders: %s\n" "$(find /_vols/* -maxdepth 1 -type d)" && \
+printf "Copying node_modules to folders:\n%s\n" "$(find /_vols/* -maxdepth 0 -type d)" && \
 chown -R 1000.1000 /app && \
-find /_vols/* -print0 -maxdepth 1 -type d | xargs -I {} rm -rf "{}/*" && \
-find /_vols/* -print0 -maxdepth 1 -type d | xargs -I {} cp -R /app/node_modules/* "{}/" > /dev/null
+find /_vols/* -print0 -mindepth 1 -maxdepth 1 -type d | xargs -0 -I {} rm -rf "{}" && \
+find /_vols/* -print0 -maxdepth 0 -type d | xargs -0 -I {} cp -R /app/node_modules/. "{}/" > /dev/null
